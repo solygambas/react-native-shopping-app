231a6340ec42e3f488b181fe2976004018b3f607
solygambas <51904909+solygambas@users.noreply.github.com>
2021-11-28 11:30:40 +0100

    add useStae

--- 00-playground/02-my-first-project/App.js
+++ 00-playground/02-my-first-project/App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Sara");
+  const [person, setPerson] = useState({ name: "Mario", age: 40 });
+
+  const clickHandler = () => {
+    setName("John");
+    setPerson({ name: "Luigi", age: 45 });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, world!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>
+          Lorem <Text>ipsum</Text> dolor sit amet.
+        </Text>
+        <Text>My name is {name}.</Text>
+        <Text>
+          His name is {person.name} and is age is {person.age}.
+        </Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+        <View style={styles.buttonContainer}>
+          <Button title="Update State" onPress={clickHandler} />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +40,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
--- 00-playground/02-my-first-project/README.md
+++ 00-playground/02-my-first-project/README.md
@@ -5,5 +5,7 @@ Another quick intro to React Native.
 ## Features
 
 - setting up Expo Go on Android.
+- using View, Text and StyleSheet.
+- handling state.
 
 Based on [React Native Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ) by Shaun Pelling - The Net Ninja (2019).
