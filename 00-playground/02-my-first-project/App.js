b33a4781f711120f12d82b64c303121e7c670467
solygambas <51904909+solygambas@users.noreply.github.com>
2021-11-28 12:33:42 +0100

    add lists and deleting items

--- 00-playground/02-my-first-project/App.js
+++ 00-playground/02-my-first-project/App.js
@@ -1,33 +1,94 @@
+// Reference: https://reactnative.dev/docs/textinput
+
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  // Button,
+  // TextInput,
+  // ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Sara");
-  const [person, setPerson] = useState({ name: "Mario", age: 40 });
+  // const [person, setPerson] = useState({ name: "Mario", age: 40 });
+  // const [name, setName] = useState("Sara");
+  // const [age, setAge] = useState("30");
+  const [people, setPeople] = useState([
+    { name: "Shaun", id: "1" },
+    { name: "Yoshi", id: "2" },
+    { name: "Mario", id: "3" },
+    { name: "Luigi", id: "4" },
+    { name: "Peach", id: "5" },
+    { name: "Toad", id: "6" },
+    { name: "Bowser", id: "7" },
+  ]);
 
-  const clickHandler = () => {
-    setName("John");
-    setPerson({ name: "Luigi", age: 45 });
+  // const clickHandler = () => {
+  // setName("John");
+  // setPerson({ name: "Luigi", age: 45 });
+  // };
+
+  const pressHandler = (id) => {
+    // console.log(id);
+    setPeople((prevPeople) => prevPeople.filter((person) => person.id != id));
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
+      {/* <View style={styles.header}>
         <Text style={styles.boldText}>Hello, world!</Text>
-      </View>
+      </View> */}
       <View style={styles.body}>
-        <Text style={styles.boldText}>
-          Lorem <Text>ipsum</Text> dolor sit amet.
-        </Text>
-        <Text>My name is {name}.</Text>
-        <Text>
+        {/* BASICS */}
+        {/* <Text style={styles.boldText}> */}
+        {/* Lorem <Text>ipsum</Text> dolor sit amet. */}
+        {/* </Text> */}
+        {/* <Text>
           His name is {person.name} and is age is {person.age}.
-        </Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <View style={styles.buttonContainer}>
+        </Text> */}
+        {/* <View style={styles.buttonContainer}>
           <Button title="Update State" onPress={clickHandler} />
-        </View>
+        </View> */}
+        {/* TEXTINPUT */}
+        {/* <Text>Enter name:</Text>
+        <TextInput
+          multiline
+          style={styles.input}
+          placeholder="e.g. John Doe"
+          onChangeText={(value) => setName(value)}
+        />
+        <Text>Enter age:</Text>
+        <TextInput
+          keyboardType="numeric"
+          style={styles.input}
+          placeholder="e.g. 40"
+          onChangeText={(value) => setAge(value)}
+        />
+        <Text>
+          Name: {name}, age: {age}.
+        </Text> */}
+        {/* LISTS */}
+        {/* <ScrollView>
+          {people.map((item) => (
+            <View key={item.id}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          ))}
+        </ScrollView> */}
+        <FlatList
+          numColumns={2}
+          keyExtractor={(item) => item.id}
+          data={people}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => pressHandler(item.id)}>
+              <Text style={styles.item}>{item.name}</Text>
+            </TouchableOpacity>
+          )}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -38,8 +99,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   header: {
     backgroundColor: "pink",
@@ -49,10 +112,25 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   body: {
-    backgroundColor: "yellow",
-    padding: 20,
+    // backgroundColor: "yellow",
+    // padding: 20,
   },
-  buttonContainer: {
-    marginTop: 20,
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+  // buttonContainer: {
+  //   marginTop: 20,
+  // },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
   },
 });
--- 00-playground/02-my-first-project/README.md
+++ 00-playground/02-my-first-project/README.md
@@ -2,10 +2,16 @@
 
 Another quick intro to React Native.
 
+<p align="center">
+        <img src="screenshot.png">
+</p>
+
 ## Features
 
 - setting up Expo Go on Android.
 - using View, Text and StyleSheet.
-- handling state.
+- handling state and user inputs with TextInput.
+- displaying lists with ScrollView and FlatList.
+- deleting items on press with TouchableOpacity.
 
 Based on [React Native Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ) by Shaun Pelling - The Net Ninja (2019).
